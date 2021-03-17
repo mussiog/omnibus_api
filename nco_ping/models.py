27a79584a98b8adc01bb92a8d87b079aef958baf
mussiog <mussiog@ar.ibm.com>
2021-03-16 23:10:31 -0300

    3rd commit

--- nco_ping/models.py
+++ nco_ping/models.py
@@ -2,11 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 Class Nco_ping(models.Model):
-    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    target = models.CharField(max_lenth=100)
     delay = models.IntegerField()
     description = models.CharField(max_lenth=100)
     lastedit = models.DateTimeField(auto_now_add=True)
 
     Class Meta:
         ordering = ['-lastedit']
-
--- nco_ping/serializers.py
+++ nco_ping/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import Nco_ping
 
-class PostSerializer(serializers.ModelSerializer):
-    poster = serializers.ReadOnlyField(source='poster.username')
+class NcoPingSerializer(serializers.ModelSerializer):
+    host = serializers.ReadOnlyField(source='poster.username')
+    delay = serializers.ReadOnlyField(source='poster.id')
+    description = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'host', 'delay', 'description', 'lastedit']
