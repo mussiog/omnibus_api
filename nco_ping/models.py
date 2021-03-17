879697fdda70f7ef53c281b4fe202ccb66ac58df
mussiog <mussiog@ar.ibm.com>
2021-03-17 00:22:03 -0300

    fixed length

--- nco_ping/models.py
+++ nco_ping/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class NcoPing(models.Model):
-    host = models.CharField(max_lenth=100)
+    host = models.CharField(max_length=100)
     delay = models.IntegerField()
-    description = models.CharField(max_lenth=100)
+    description = models.CharField(max_length=100)
     lastedit = models.DateTimeField(auto_now_add=True)
 
     class Meta:
